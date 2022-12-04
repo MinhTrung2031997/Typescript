2aaaf37dc29e2382d801a9b2a1714e00ff3bb481
TrungLtv <trung@ltv.dev>
2022-12-04 21:04:07 +0700

    expert

--- src/super/satisfies_operator.ts
+++ src/super/satisfies_operator.ts
@@ -1,3 +1,12 @@
-export type Color = ColorString | ColorRGB
-export type ColorString = "red" | "blue" | "yellow" | "purple"
-export type  ColorRGB = [red: number, green: number, blue: number]
+export type ColorString = "red" | "blue" | "yellow" | "purple";
+export type ColorRGB = [red: number, green: number, blue: number];
+export type Color = ColorString | ColorRGB;
+
+export type Theme = Record<string, Color>;
+const theme = {
+  primary: "red",
+  secondary: [0, 255, 0],
+  tertiary: "purple",
+} satisfies Theme;
+
+const [r, g, h] = theme.secondary;
